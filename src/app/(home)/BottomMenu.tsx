242c83c9f83512978fb175301b32715a9d203a49
Leandro Norcio <onsongnorcio@gmail.com>
2023-06-15 00:18:25 +0800

    Minor fixes to transitions when resizing the page.

--- src/app/(home)/BottomMenu.tsx
+++ src/app/(home)/BottomMenu.tsx
@@ -15,7 +15,7 @@ export function BottomMenu() {
   const id = session?.user ? session.user.id : '/usernotfound';
 
   return (
-    <div className="transition-all duration-500 w-full fixed left-0 bottom-0 md:-bottom-16 h-14 flex flex-row bg-violet-500 shadow-2xl">
+    <div className="transition-[bottom] duration-500 w-full fixed left-0 bottom-0 md:-bottom-16 h-14 flex flex-row bg-violet-500 shadow-2xl">
       {[
         {
           Icon: GridFeedCards,
