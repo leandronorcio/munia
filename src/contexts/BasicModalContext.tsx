2ae25dcf85aa5eb9af6637c810c48993d2eebf85
Leandro Norcio <onsongnorcio@gmail.com>
2023-06-17 15:59:31 +0800

    Removed  in <BasicModalContext.tsx> as it's not necessary to show a modal.

--- src/contexts/BasicModalContext.tsx
+++ src/contexts/BasicModalContext.tsx
@@ -4,7 +4,6 @@ import Button from '@/components/ui/Button';
 import { cn } from '@/lib/cn';
 import { Close } from '@/svg_components';
 import { createContext, useState } from 'react';
-import { createPortal } from 'react-dom';
 
 const BasicModalContext = createContext<{
   shown: boolean;
@@ -81,54 +80,52 @@ function BasicModalContextProvider({
 
   return (
     <BasicModalContext.Provider value={{ shown: shown, alert, confirm }}>
-      {shown &&
-        createPortal(
+      {shown && (
+        <div
+          className={cn(
+            'fixed top-0 left-0 transition-all duration-500 w-full h-screen flex justify-center items-end md:items-center z-10 shadow-lg',
+            animation === 'from' ? 'backdrop-blur-none' : 'backdrop-blur-sm'
+          )}
+        >
           <div
             className={cn(
-              'fixed top-0 left-0 transition-all duration-500 w-full h-screen flex justify-center items-end md:items-center z-10 shadow-lg',
-              animation === 'from' ? 'backdrop-blur-none' : 'backdrop-blur-sm'
+              'transition-all duration-500 w-full px-5 md:px-32 py-14 md:py-24 md:w-3/5 lg:w-1/2 xl:w-2/5 rounded-t-3xl md:rounded-3xl bg-white flex flex-col items-center gap-6 relative',
+              animation === 'from' ? '-mt-24' : 'mt-0',
+              animation === 'from' ? 'opacity-0' : 'opacity-1'
             )}
           >
-            <div
-              className={cn(
-                'transition-all duration-500 w-full px-5 md:px-32 py-14 md:py-24 md:w-3/5 lg:w-1/2 xl:w-2/5 rounded-t-3xl md:rounded-3xl bg-white flex flex-col items-center gap-6 relative',
-                animation === 'from' ? '-mt-24' : 'mt-0',
-                animation === 'from' ? 'opacity-0' : 'opacity-1'
-              )}
+            <Close
+              className="absolute top-6 md:top-8 right-6 md:right-8 cursor-pointer stroke-gray-900 hover:stroke-gray-500"
+              width={24}
+              height={24}
+              onClick={hide}
+            />
+            <h1 className="text-center text-4xl md:text-5xl font-bold">
+              {dialog.title}
+            </h1>
+            <p className="text-lg text-center text-gray-700">
+              {dialog.message}
+            </p>
+            <Button
+              onClick={() => {
+                hide();
+                dialog.type === 'alert'
+                  ? hide()
+                  : dialog.actionOnConfirm && dialog.actionOnConfirm();
+              }}
+              shape="pill"
+              size="long"
             >
-              <Close
-                className="absolute top-6 md:top-8 right-6 md:right-8 cursor-pointer stroke-gray-900 hover:stroke-gray-500"
-                width={24}
-                height={24}
-                onClick={hide}
-              />
-              <h1 className="text-center text-4xl md:text-5xl font-bold">
-                {dialog.title}
-              </h1>
-              <p className="text-lg text-center text-gray-700">
-                {dialog.message}
-              </p>
-              <Button
-                onClick={() => {
-                  hide();
-                  dialog.type === 'alert'
-                    ? hide()
-                    : dialog.actionOnConfirm && dialog.actionOnConfirm();
-                }}
-                shape="pill"
-                size="long"
-              >
-                {dialog.type === 'alert' ? 'Okay' : 'Confirm'}
+              {dialog.type === 'alert' ? 'Okay' : 'Confirm'}
+            </Button>
+            {dialog.type === 'confirm' && (
+              <Button onClick={hide} shape="pill" size="long" mode="ghost">
+                Cancel
               </Button>
-              {dialog.type === 'confirm' && (
-                <Button onClick={hide} shape="pill" size="long" mode="ghost">
-                  Cancel
-                </Button>
-              )}
-            </div>
-          </div>,
-          document.body
-        )}
+            )}
+          </div>
+        </div>
+      )}
       {children}
     </BasicModalContext.Provider>
   );
