a7beaeb04591b77e52eb1e2027fc38d292863ea2
Leandro Norcio <onsongnorcio@gmail.com>
2023-06-19 00:38:42 +0800

    Instead of scaling a post's video when hovered, it will scale the Play icon instead due to the bug that causes the z-index to be messed up when scaling an element.

--- src/components/PostVisualMedia.tsx
+++ src/components/PostVisualMedia.tsx
@@ -11,7 +11,7 @@ export default function PostVisualMedia({
 }) {
   return (
     <div
-      className="w-1/2 max-h-[360px] cursor-pointer overflow-hidden relative"
+      className="w-1/2 max-h-[360px] cursor-pointer overflow-hidden relative group"
       onClick={onClick}
     >
       {type === 'photo' ? (
@@ -22,15 +22,11 @@ export default function PostVisualMedia({
       ) : (
         <>
           <Play
-            stroke="white"
             width={48}
             height={48}
-            className="absolute z-100 top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%]"
+            className="stroke-violet-100 absolute top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%] transition-transform group-hover:scale-125"
           />
-          <video
-            className="object-cover w-full h-full transition-transform hover:scale-110 z-10"
-            style={{ transformStyle: 'flat' }}
-          >
+          <video className="object-cover w-full h-full">
             <source src={url} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
