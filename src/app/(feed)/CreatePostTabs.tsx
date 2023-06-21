6514103f1edc5999b59439469aacc1acec18a318
Leandro Norcio <onsongnorcio@gmail.com>
2023-06-22 01:09:39 +0800

    * Memoize the generated VisualMedia[] within <CreatePostTabs.tsx> because <URL.createObjectURL> keeps creating a new URL every render. * Memoize <PostVisualMediaContainer.tsx> to prevent expensive rerenders of large visual media assets.

--- src/app/(feed)/CreatePostTabs.tsx
+++ src/app/(feed)/CreatePostTabs.tsx
@@ -2,65 +2,72 @@ import PostVisualMediaContainer from '@/components/PostVisualMediaContainer';
 import { capitalizeFirstLetter } from '@/lib/capitalizeFirstLettet';
 import { cn } from '@/lib/cn';
 import { Ellipse } from '@/svg_components';
-import { memo, useState } from 'react';
+import { memo, useMemo, useState } from 'react';
 
 const CreatePostTabs = memo(
   function CreatePostTabs({ visualMediaFiles }: { visualMediaFiles: File[] }) {
     const [activeCreatePostTab, setActiveCreatePostTab] = useState<
       'preview' | 'sort'
     >('preview');
-    console.log('CreatePostTabs rendered');
+
+    // Memoize the generated VisualMedia[] because <URL.createObjectURL> keeps creating a new URL every render.
+    const visualMedia: VisualMedia[] = useMemo(
+      () =>
+        visualMediaFiles.map((file) => ({
+          type: file.type.startsWith('image/') ? 'PHOTO' : 'VIDEO',
+          url: URL.createObjectURL(file),
+        })),
+      [visualMediaFiles]
+    );
     return (
       <>
-        {visualMediaFiles.length > 0 && (
-          <div className="overflow-x-hidden border-t-2 mt-4">
-            <div className="flex justify-center">
-              {['preview', 'sort'].map((item, i) => {
-                // Hide the Sort tab when there is only one selected file.
-                if (visualMediaFiles.length === 1 && i === 1) return false;
-                const isActive = item === activeCreatePostTab;
-                return (
-                  <div
-                    className="flex flex-col gap-2 items-center px-3 py-4 cursor-pointer"
-                    onClick={() =>
-                      setActiveCreatePostTab(item as 'preview' | 'sort')
-                    }
-                    key={item}
+        <div
+          className={cn(
+            'overflow-x-hidden border-t-2 mt-4',
+            visualMediaFiles.length > 0 ? 'block' : 'hidden'
+          )}
+        >
+          <div className="flex justify-center">
+            {['preview', 'sort'].map((item, i) => {
+              // Hide the Sort tab when there is only one selected file.
+              if (visualMediaFiles.length === 1 && i === 1) return false;
+              const isActive = item === activeCreatePostTab;
+              return (
+                <div
+                  className="flex flex-col gap-2 items-center px-3 py-4 cursor-pointer"
+                  onClick={() =>
+                    setActiveCreatePostTab(item as 'preview' | 'sort')
+                  }
+                  key={item}
+                >
+                  <h2
+                    className={cn(
+                      'text-xl font-semibold',
+                      isActive
+                        ? 'text-black'
+                        : 'text-gray-500 hover:text-gray-700'
+                    )}
                   >
-                    <h2
-                      className={cn(
-                        'text-xl font-semibold',
-                        isActive
-                          ? 'text-black'
-                          : 'text-gray-500 hover:text-gray-700'
-                      )}
-                    >
-                      {capitalizeFirstLetter(item)}
-                    </h2>
-                    {isActive && <Ellipse width={8} height={8} />}
-                  </div>
-                );
-              })}
-            </div>
-
-            <div
-              className={cn(
-                'flex w-[200%] transition-transform duration-500',
-                activeCreatePostTab === 'sort' && 'translate-x-[-50%]'
-              )}
-            >
-              <div className="bg-red-500 basis-1/2">
-                <PostVisualMediaContainer
-                  visualMedia={visualMediaFiles.map((file) => ({
-                    type: file.type.startsWith('image/') ? 'PHOTO' : 'VIDEO',
-                    url: URL.createObjectURL(file),
-                  }))}
-                />
-              </div>
-              <div className="bg-green-500 basis-1/2"></div>
-            </div>
+                    {capitalizeFirstLetter(item)}
+                  </h2>
+                  {isActive && <Ellipse width={8} height={8} />}
+                </div>
+              );
+            })}
           </div>
-        )}
+
+          <div
+            className={cn(
+              'flex w-[200%] transition-transform duration-500',
+              activeCreatePostTab === 'sort' && 'translate-x-[-50%]'
+            )}
+          >
+            <div className="basis-1/2">
+              <PostVisualMediaContainer visualMedia={visualMedia} />
+            </div>
+            <div className="basis-1/2"></div>
+          </div>
+        </div>
       </>
     );
   },
@@ -80,17 +87,9 @@ const CreatePostTabs = memo(
       }
 
       // Check if the properties of the files match
-      if (oldFile.name !== newFile.name) {
-        return false;
-      }
-
-      if (oldFile.size !== newFile.size) {
-        return false;
-      }
-
-      if (oldFile.type !== newFile.type) {
-        return false;
-      }
+      if (oldFile.name !== newFile.name) return false;
+      if (oldFile.size !== newFile.size) return false;
+      if (oldFile.type !== newFile.type) return false;
     }
 
     return true;
--- src/components/PostVisualMediaContainer.tsx
+++ src/components/PostVisualMediaContainer.tsx
@@ -1,44 +1,63 @@
-import { useContext } from 'react';
+import { memo, useContext } from 'react';
 import PostVisualMedia from './PostVisualMedia';
 import { VisualMediaModalContext } from '@/contexts/VisualMediaModalContext';
 import { cn } from '@/lib/cn';
 import { isOdd } from '@/lib/isOdd';
 
-export default function PostVisualMediaContainer({
-  visualMedia,
-}: {
-  visualMedia: VisualMedia[];
-}) {
-  const { showVisualMediaModal } = useContext(VisualMediaModalContext);
-  const len = visualMedia.length;
-  console.log('PostVisualMediaContainer rendered');
-  return (
-    <div
-      className={cn('grid relative', len > 2 ? 'grid-cols-2' : 'grid-cols-1')}
-    >
-      {visualMedia.length > 0 &&
-        visualMedia.map((item, i) => {
-          // Only display four images.
-          if (i > 3) return false;
-          return (
-            <PostVisualMedia
-              key={i}
-              type={item.type}
-              url={item.url}
-              height={len > 1 ? '300px' : '480px'}
-              // If odd and len is < 4, the first image must take the full width.
-              colSpan={isOdd(len) && len < 4 && i === 0 ? 2 : 1}
-              onClick={() =>
-                showVisualMediaModal({ visualMedia, initialSlide: i })
-              }
-            />
-          );
-        })}
-      {len > 4 && (
-        <p className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-5xl text-white font-semibold">
-          +{len - 4}
-        </p>
-      )}
-    </div>
-  );
-}
+const PostVisualMediaContainer = memo(
+  function PostVisualMediaContainer({
+    visualMedia,
+  }: {
+    visualMedia: VisualMedia[];
+  }) {
+    const { showVisualMediaModal } = useContext(VisualMediaModalContext);
+    const len = visualMedia.length;
+
+    return (
+      <div
+        className={cn('grid relative', len > 2 ? 'grid-cols-2' : 'grid-cols-1')}
+      >
+        {visualMedia.length > 0 &&
+          visualMedia.map((item, i) => {
+            // Only display four images.
+            if (i > 3) return false;
+            return (
+              <PostVisualMedia
+                key={i}
+                type={item.type}
+                url={item.url}
+                height={len > 1 ? '300px' : '480px'}
+                // If odd and len is < 4, the first image must take the full width.
+                colSpan={isOdd(len) && len < 4 && i === 0 ? 2 : 1}
+                onClick={() =>
+                  showVisualMediaModal({ visualMedia, initialSlide: i })
+                }
+              />
+            );
+          })}
+        {len > 4 && (
+          <p className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-5xl text-white font-semibold">
+            +{len - 4}
+          </p>
+        )}
+      </div>
+    );
+  },
+  (oldProps, newProps) => {
+    const oldFiles = oldProps.visualMedia;
+    const newFiles = newProps.visualMedia;
+
+    if (oldFiles.length !== newFiles.length) return false;
+
+    for (let i = 0; i < oldFiles.length; i++) {
+      const oldFile = oldFiles[i];
+      const newFile = newFiles[i];
+
+      if (oldFile.type !== newFile.type) return false;
+      if (oldFile.url !== newFile.url) return false;
+    }
+    return true;
+  }
+);
+
+export default PostVisualMediaContainer;
