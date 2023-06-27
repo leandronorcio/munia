e542a17a8ee6e3bb922c04a4150e159ddafe588f
Leandro Norcio <onsongnorcio@gmail.com>
2023-06-27 14:23:44 +0800

    Added inifinte loading of posts.

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,7 +1,8 @@
 'use client';
 import { ToastContext } from '@/contexts/ToastContext';
-import { useContext, useEffect, useState } from 'react';
-import Post from './Post';
+import { useContext, useEffect, useRef, useState } from 'react';
+import { Post } from './Post';
+import useOnScreen from '@/hooks/useOnScreen';
 
 export function Posts({
   type,
@@ -12,39 +13,62 @@ export function Posts({
 }) {
   const [posts, setPosts] = useState<PostType[]>([]);
   const [pagination, setPagination] = useState(0);
+  const [maxedOut, setMaxedOut] = useState(false);
   const { toastify } = useContext(ToastContext);
+  const loadingElRef = useRef<HTMLDivElement>(null);
+  const isBottomOnScreen = useOnScreen(loadingElRef);
+
+  const retrievePosts = async () => {
+    if (maxedOut) return;
+    const postsPerPage = 3;
+    const limit = postsPerPage;
+    const offset = pagination * postsPerPage;
+    const url = new URL(
+      document.location.origin + `/api/users/${userId}/posts`
+    );
+    url.searchParams.set('limit', limit.toString());
+    url.searchParams.set('offset', offset.toString());
+
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      toastify({
+        type: 'error',
+        title: 'Error',
+        message: 'There was an error getting the posts.',
+      });
+    }
+
+    const { posts: retrievedPosts } = (await res.json()) as {
+      posts: PostType[];
+    };
+    if (retrievedPosts.length === 0) {
+      setMaxedOut(true);
+      return;
+    }
+    setPosts((prev) => [...prev, ...retrievedPosts]);
+  };
 
   useEffect(() => {
-    const run = async () => {
-      const limit = 5;
-      const offset = pagination * 5;
-      const url = new URL(
-        document.location.origin + `/api/users/${userId}/posts`
-      );
-      url.searchParams.set('limit', limit.toString());
-      url.searchParams.set('offset', offset.toString());
-
-      const res = await fetch(url);
-
-      if (!res.ok) {
-        toastify({
-          type: 'error',
-          title: 'Error',
-          message: 'There was an error getting the posts.',
-        });
-      }
-
-      const { posts: retrievedPosts } = await res.json();
-      setPosts(retrievedPosts);
-    };
-    run();
+    retrievePosts();
   }, [pagination]);
+
+  useEffect(() => {
+    if (isBottomOnScreen === true) setPagination((prev) => prev + 1);
+  }, [isBottomOnScreen]);
 
   return (
     <div className="flex flex-col gap-8">
       {posts.map((post) => {
         return <Post key={post.id} {...post} />;
       })}
+      <div onClick={() => setPagination((prev) => prev + 1)} ref={loadingElRef}>
+        {maxedOut
+          ? pagination === 1
+            ? 'No posts yet'
+            : 'All posts loaded'
+          : 'Loading posts...'}
+      </div>
     </div>
   );
 }
---
+++ src/hooks/useOnScreen.tsx
@@ -0,0 +1,29 @@
+import { RefObject, useEffect, useState } from 'react';
+
+const useOnScreen = (ref: RefObject<HTMLDivElement>, rootMargin = '0px') => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      {
+        rootMargin,
+      }
+    );
+
+    const currentElement = ref?.current;
+    if (currentElement == null) return;
+
+    observer.observe(currentElement);
+
+    return () => {
+      observer.unobserve(currentElement);
+    };
+  }, []);
+
+  return isVisible;
+};
+
+export default useOnScreen;
