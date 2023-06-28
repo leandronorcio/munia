33094750c2f7d2e29ac12af052d8eae8b1047c22
Leandro Norcio <onsongnorcio@gmail.com>
2023-06-29 00:58:17 +0800

    Removed handle field from the User model.

--- types/globals.d.ts
+++ types/globals.d.ts
@@ -3,7 +3,6 @@ import type { DefaultUser } from 'next-auth';
 
 declare global {
   interface CustomUser extends DefaultUser {
-    handle: string;
     profilePhoto: string;
     coverPhoto: string;
   }
