cfe63ec68191c897a012502f68aeab3e16db28db
Leandro Norcio <onsongnorcio@gmail.com>
2023-06-28 20:30:14 +0800

    Fixed <ProfilePhoto> component.

--- src/components/ui/ProfilePhoto.tsx
+++ src/components/ui/ProfilePhoto.tsx
@@ -5,15 +5,15 @@ export default function ProfilePhoto() {
   const { data: session } = useSession();
 
   return (
-    <div
-      className="w-full h-full rounded-full bg-cover"
-      style={{
-        backgroundImage: `url("${
+    <div className="w-full h-full rounded-full">
+      <img
+        src={
           session?.user?.profilePhoto
             ? session.user.profilePhoto
             : '/default-profile-photo.jpg'
-        }")`,
-      }}
-    ></div>
+        }
+        className="w-full h-full object-cover rounded-full"
+      />
+    </div>
   );
 }
