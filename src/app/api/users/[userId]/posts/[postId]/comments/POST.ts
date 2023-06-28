2867326b621bf56403af7d7d70fdf6ab882855e0
Leandro Norcio <onsongnorcio@gmail.com>
2023-06-29 01:10:19 +0800

    Fixed comments POST handler not able to write comments on other users' posts.

--- src/app/api/users/[userId]/posts/[postId]/comments/POST.ts
+++ src/app/api/users/[userId]/posts/[postId]/comments/POST.ts
@@ -7,8 +7,7 @@ export async function POST(
   { params }: { params: { userId: string; postId: string } }
 ) {
   const [user] = await useProtectApiRoute();
-  if (!user || user.id !== params.userId)
-    return NextResponse.json({}, { status: 401 });
+  if (!user) return NextResponse.json({}, { status: 401 });
 
   const { content } = (await request.json()) as CommentPOSTRequestBody;
 
