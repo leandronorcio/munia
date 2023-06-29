159ab5558f408f62c115562534b1ebab04f0f1c1
Leandro Norcio <onsongnorcio@gmail.com>
2023-06-29 23:01:53 +0800

    The <ModalWrapper> no longer aligns its children, its children must align itself according to its needs.

--- src/components/ModalContentWrapper.tsx
+++ src/components/ModalContentWrapper.tsx
@@ -7,7 +7,7 @@ export default function ModalContentWrapper({
 }) {
   return (
     <motion.div
-      className="w-full px-5 md:px-32 py-14 md:py-24 md:w-3/5 lg:w-1/2 xl:w-2/5 rounded-t-3xl md:rounded-3xl bg-white flex flex-col items-center gap-6 relative"
+      className="w-full mx-auto top-[100%] translate-y-[-100%] md:top-[50%] md:translate-y-[-50%] px-5 md:px-32 py-14 md:py-24 md:w-3/5 lg:w-1/2 xl:w-2/5 rounded-t-3xl md:rounded-3xl bg-white flex flex-col items-center gap-6 relative"
       initial={{ marginTop: '-96px' }}
       animate={{ marginTop: '0px' }}
       exit={{ marginTop: '-96px' }}
--- src/components/ModalWrapper.tsx
+++ src/components/ModalWrapper.tsx
@@ -12,7 +12,7 @@ export default function ModalWrapper({
       initial={{ backdropFilter: 'blur(0)', opacity: 0 }}
       animate={{ backdropFilter: 'blur(4px)', opacity: 1 }}
       exit={{ backdropFilter: 'blur(0)', opacity: 0 }}
-      className="fixed top-0 left-0 w-full h-screen flex justify-center items-end md:items-center"
+      className="fixed top-0 left-0 w-full h-screen"
       style={{ zIndex }}
     >
       {children}
