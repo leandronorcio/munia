d444c2e0842bdd2e424b05337d4a7d2e578e5913
Leandro Norcio <onsongnorcio@gmail.com>
2023-07-03 02:06:10 +0800

    Refactor <TextArea> component to support ref opt-in.

--- src/components/ui/TextArea.tsx
+++ src/components/ui/TextArea.tsx
@@ -2,30 +2,33 @@
 'use client';
 
 import { cn } from '@/lib/cn';
-import { useRef } from 'react';
+import { forwardRef, useRef } from 'react';
 
 interface TextAreaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
   // put custom props here
 }
-export default function TextArea({ ...props }: TextAreaProps) {
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
-  return (
-    <textarea
-      ref={textareaRef}
-      onInput={() => {
-        const textarea = textareaRef.current;
-        if (textarea === null) return;
+export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ ...props }, ref) => {
+    const localRef = useRef<HTMLTextAreaElement>(null);
+    const textareaRef = ref || localRef;
 
-        textarea.style.height = 'auto';
-        textarea.style.height = textarea.scrollHeight + 'px';
-      }}
-      {...props}
-      className={cn(
-        'w-full bg-transparent outline-none px-4 py-0 text-lg overflow-hidden resize-none',
-        props.className
-      )}
-      rows={1}
-    />
-  );
-}
+    return (
+      <textarea
+        ref={textareaRef}
+        onInput={(e) => {
+          const textarea = e.target as HTMLTextAreaElement;
+
+          textarea.style.height = 'auto';
+          textarea.style.height = textarea.scrollHeight + 'px';
+        }}
+        {...props}
+        className={cn(
+          'w-full bg-transparent outline-none px-4 py-0 text-lg overflow-hidden resize-none',
+          props.className
+        )}
+        rows={1}
+      />
+    );
+  }
+);
