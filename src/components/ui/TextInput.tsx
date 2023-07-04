1f81db6187338b81dc9d9049dc96dc3ba7f2bc95
Leandro Norcio <onsongnorcio@gmail.com>
2023-07-04 13:28:12 +0800

    Added support for icons and ref to the <TextInput> component.

--- src/components/ui/TextInput.tsx
+++ src/components/ui/TextInput.tsx
@@ -1,21 +1,41 @@
+import { cn } from '@/lib/cn';
+import { SVGProps, forwardRef, useRef } from 'react';
+
 interface TextInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
+  Icon?: (props: SVGProps<SVGSVGElement>) => JSX.Element;
 }
-export function TextInput({ label, ...rest }: TextInputProps) {
-  return (
-    <div className="relative">
-      <input
-        {...rest}
-        className="w-[320px] pt-8 py-2 px-5 bg-slate-100 outline-none rounded-2xl focus:ring-2 ring-black peer"
-        placeholder=" "
-        id={rest.id || 'generic-input'}
-      />
-      <label
-        htmlFor={rest.id || 'generic-input'}
-        className="cursor-text absolute z-0 left-5 transition-all text-gray-500 top-[9px] translate-y-0 text-sm peer-focus:top-[9px] peer-focus:translate-y-0 peer-focus:text-sm peer-placeholder-shown:top-[50%] peer-placeholder-shown:translate-y-[-50%] peer-placeholder-shown:text-lg"
-      >
-        {label}
-      </label>
-    </div>
-  );
-}
+export const TextInput = forwardRef<HTMLInputElement, TextInputProps>(
+  ({ label, Icon, ...rest }, ref) => {
+    const localRef = useRef<HTMLInputElement>(null);
+    const inputRef = ref || localRef;
+    return (
+      <div className="relative">
+        {Icon && (
+          <div className="absolute top-[50%] translate-y-[-50%] left-5">
+            <Icon className="stroke-gray-500" width={24} height={24} />
+          </div>
+        )}
+        <input
+          {...rest}
+          className={cn(
+            'w-[320px] pt-8 pb-2 pr-5 bg-slate-100 outline-none rounded-2xl focus:ring-2 ring-black peer',
+            Icon ? 'pl-16' : 'pl-5'
+          )}
+          placeholder=" "
+          id={rest.id || 'generic-input'}
+          ref={inputRef}
+        />
+        <label
+          htmlFor={rest.id || 'generic-input'}
+          className={cn(
+            'cursor-text absolute z-0 transition-all text-gray-500 top-[9px] translate-y-0 text-sm peer-focus:top-[9px] peer-focus:translate-y-0 peer-focus:text-sm peer-placeholder-shown:top-[50%] peer-placeholder-shown:translate-y-[-50%] peer-placeholder-shown:text-lg',
+            Icon ? 'left-16' : 'left-5'
+          )}
+        >
+          {label}
+        </label>
+      </div>
+    );
+  }
+);
