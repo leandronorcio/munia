0044fc7186e1e3b36d51839a55f1e4b91d0251cc
Leandro Norcio <onsongnorcio@gmail.com>
2023-07-07 01:47:57 +0800

    The textarea now has a <filled> and <error> prop.

--- src/components/ui/TextArea.tsx
+++ src/components/ui/TextArea.tsx
@@ -6,28 +6,35 @@ import { forwardRef, useRef } from 'react';
 interface TextAreaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
   // put custom props here
+  filled?: boolean;
+  error?: string;
 }
 export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
-  ({ ...props }, ref) => {
+  ({ filled = false, error, ...rest }, ref) => {
     const localRef = useRef<HTMLTextAreaElement>(null);
     const textareaRef = ref || localRef;
 
     return (
-      <textarea
-        ref={textareaRef}
-        onInput={(e) => {
-          const textarea = e.target as HTMLTextAreaElement;
+      <div>
+        <textarea
+          ref={textareaRef}
+          onInput={(e) => {
+            const textarea = e.target as HTMLTextAreaElement;
 
-          textarea.style.height = 'auto';
-          textarea.style.height = textarea.scrollHeight + 'px';
-        }}
-        {...props}
-        className={cn(
-          'w-full bg-transparent outline-none px-4 py-0 text-lg overflow-hidden resize-none',
-          props.className
-        )}
-        rows={1}
-      />
+            textarea.style.height = 'auto';
+            textarea.style.height = textarea.scrollHeight + 'px';
+          }}
+          {...rest}
+          className={cn(
+            'w-full  outline-none px-4 py-0 text-lg overflow-hidden resize-none',
+            filled ? 'p-5 rounded-2xl bg-slate-100' : 'bg-transparent',
+            error && 'bg-red-200 ring-2 ring-red-900 placeholder:text-red-900',
+            rest.className
+          )}
+          rows={1}
+        />
+        {error && <p className="text-red-800 font-semibold mt-2">{error}</p>}
+      </div>
     );
   }
 );
