45100da354451c70a33227db765d7fd5a1b39e89
Leandro Norcio <onsongnorcio@gmail.com>
2023-07-07 14:32:15 +0800

    Made the <ProfileActionButtons> responsive.

--- src/app/[userId]/ProfileActionButtons.tsx
+++ src/app/[userId]/ProfileActionButtons.tsx
@@ -4,11 +4,11 @@ import { ActionsPlus, Mail } from '@/svg_components';
 
 export function ProfileActionButtons() {
   return (
-    <div className="absolute -bottom-14 right-4 flex flex-row gap-2 sm:gap-4">
-      <Button Icon={Mail} onClick={() => {}} size="small" mode="secondary" />
-      <Button Icon={ActionsPlus} onClick={() => {}} size="small">
+    <div className="absolute -bottom-16 right-2 md:right-0 flex flex-row items-center gap-2 md:gap-4">
+      <Button Icon={ActionsPlus} onClick={() => {}} shape="pill" size="small">
         Follow
       </Button>
+      <Button Icon={Mail} onClick={() => {}} mode="secondary" size="small" />
     </div>
   );
 }
