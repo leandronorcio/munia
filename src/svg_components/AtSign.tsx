2677e147dc179b7ca18cc0e3fffa7fbd29d314d2
Leandro Norcio <onsongnorcio@gmail.com>
2023-07-10 02:43:49 +0800

    Added SVG icons.

---
+++ src/svg_components/AtSign.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { SVGProps } from 'react';
+const SvgAtSign = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      stroke={props.stroke}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M17.5 20.353A9.954 9.954 0 0 1 12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10a3 3 0 1 1-6 0m0-4v5m0-1a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z"
+    />
+  </svg>
+);
+export default SvgAtSign;
---
+++ src/svg_components/Facebook.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { SVGProps } from "react";
+const SvgFacebook = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="currentColor"
+    aria-hidden="true"
+    viewBox="0 0 8 19"
+    width="1em"
+    height="1em"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgFacebook;
