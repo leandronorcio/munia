d406268cf99ddb408b24b105f4496f4bc04a5cff
Leandro Norcio <onsongnorcio@gmail.com>
2023-07-25 01:48:42 +0800

    Fix useUserData() returning undefined by updating query key to include user's ID.

--- src/hooks/useUserData.ts
+++ src/hooks/useUserData.ts
@@ -7,16 +7,19 @@ import { GetUser } from 'types';
  * Use this hook to get the data of the current logged in user.
  *
  * NOTE: If you only need to get the `id`, `name`, `email` and `image`
- * of the user, use NextAuth's useSession()` instead.
+ * of the user, use NextAuth's `useSession()` instead.
  *
- * @returns @typedef GetUser
+ * @returns GetUser
  */
-export async function useUserData() {
+
+export function useUserData() {
   const { data: session } = useSession();
+  const userId = session?.user.id;
+
   const { data } = useQuery({
-    queryKey: ['session'],
+    queryKey: ['session-user-data', userId],
     queryFn: async () => {
-      const res = await fetch(`/api/users/${session?.user.id}`);
+      const res = await fetch(`/api/users/${userId}`);
 
       if (!res) {
         throw new Error("Error getting logged in user's data.");
@@ -24,8 +27,8 @@ export async function useUserData() {
 
       return (await res.json()) as GetUser;
     },
-    staleTime: 1000 * 60 * 5,
+    staleTime: 1000 * 60 * 10,
   });
 
-  return { userData: data };
+  return [data];
 }
