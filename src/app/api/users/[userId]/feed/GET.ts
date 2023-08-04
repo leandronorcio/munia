2ac408ffd2dda526a06634abff556d54644758a6
Leandro Norcio <onsongnorcio@gmail.com>
2023-08-04 20:13:00 +0800

    Added API route handler for getting the user's feed (i.e. the user's posts and posts of their followed users).

--- src/app/api/users/[userId]/feed/GET.ts
+++ src/app/api/users/[userId]/feed/GET.ts
@@ -1,10 +1,58 @@
 /**
  * GET /api/users/:userId/feed
  * - Allows an authenticated user to retrieve the most recent posts
- * posted by the user and their followers.
+ * posted by the user and their followed users.
  */
+
+import { getServerUser } from '@/lib/getServerUser';
+import prisma from '@/lib/prisma/prisma';
+import { selectPost } from '@/lib/prisma/selectPost';
+import { toGetPost } from '@/lib/prisma/toGetPost';
+import { NextResponse } from 'next/server';
+import { GetPost } from 'types';
 
 export async function GET(
   request: Request,
   { params }: { params: { userId: string } },
-) {}
+) {
+  const [user] = await getServerUser();
+  if (!user || params.userId !== user.id)
+    return NextResponse.json({}, { status: 401 });
+
+  const { searchParams } = new URL(request.url);
+  const limit = parseInt(searchParams.get('limit') || '5');
+  const cursor = parseInt(searchParams.get('cursor') || '0');
+
+  // Get the IDs of the user's followed users
+  const following = await prisma.follow.findMany({
+    where: {
+      followerId: user.id,
+    },
+    select: {
+      followingId: true,
+    },
+  });
+  const followingIds = following.map((user) => user.followingId);
+
+  const res = await prisma.post.findMany({
+    where: {
+      userId: {
+        in: [...followingIds, user.id],
+      },
+    },
+    select: selectPost(user.id),
+    take: limit,
+    skip: cursor ? 1 : undefined,
+    cursor: cursor
+      ? {
+          id: cursor,
+        }
+      : undefined,
+    orderBy: {
+      id: 'desc',
+    },
+  });
+
+  const posts = res.map((post) => toGetPost(post));
+  return NextResponse.json<GetPost[]>(posts);
+}
