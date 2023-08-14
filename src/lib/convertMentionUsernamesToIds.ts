169b8078b708768bef3b1ec16e41bc63430dece1
Leandro Norcio <onsongnorcio@gmail.com>
2023-08-15 00:37:57 +0800

    Revise `convertMentionUsernamesToIds()` to also return an array of mentioned users.

--- src/lib/convertMentionUsernamesToIds.ts
+++ src/lib/convertMentionUsernamesToIds.ts
@@ -9,7 +9,7 @@ import prisma from './prisma/prisma';
  *
  * @param str The string to process.
  * @param reverse Whether to convert the other way around - `id` to `username`.
- * @returns The processed string.
+ * @returns { str: string; usersMentioned?: { id: string; username: string | null; }[]; }
  */
 export async function convertMentionUsernamesToIds({
   str,
@@ -17,17 +17,24 @@ export async function convertMentionUsernamesToIds({
 }: {
   str: string;
   reverse?: boolean;
-}) {
+}): Promise<{
+  str: string;
+  usersMentioned?: {
+    id: string;
+    username: string | null;
+  }[];
+}> {
   const pattern = /(^|\s)(@)(\w+|\w+)/g;
-  const matches = str.match(pattern)?.map((match) => match.slice(2));
+  const matches = str
+    .match(pattern)
+    ?.map((match) => match.slice(match.charAt(1) === '@' ? 2 : 1));
 
   // If there are no `@` mentions return the original string
-  if (!matches) return str;
+  if (!matches) return { str };
 
   // The `matchesUnique` can either be the id/username of the mentioned users
   const matchesUnique = uniq(matches);
-
-  const users = await prisma.user.findMany({
+  const usersMentioned = await prisma.user.findMany({
     where: {
       ...(!reverse
         ? {
@@ -49,13 +56,16 @@ export async function convertMentionUsernamesToIds({
 
   // Replace the matches with the id/username of the users
   const res = str.replace(pattern, (match, space, char, word) => {
-    const user = users.find(
+    const user = usersMentioned.find(
       (user) => (!reverse ? user.username : user.id) === word,
     );
     return `${space}${char}${
-      user ? (reverse ? user.username : user.id) : word
+      user ? (!reverse ? user.id : user.username) : word
     }`;
   });
 
-  return res;
+  return {
+    str: res,
+    usersMentioned,
+  };
 }
