2d029c75e04b60336d79b0075dae621033a55c93
Leandro Norcio <onsongnorcio@gmail.com>
2023-08-15 00:47:06 +0800

    Added `mentionsActivityLogger()` for handling the logging of 'POST_MENTION', 'COMMENT_MENTION' and 'REPLY_MENTION' activities.

---
+++ src/lib/mentionsActivityLogger.ts
@@ -0,0 +1,40 @@
+import 'server-only';
+import prisma from './prisma/prisma';
+
+export async function mentionsActivityLogger({
+  usersMentioned,
+  activity,
+  isUpdate,
+}: {
+  usersMentioned?: {
+    id: string;
+    username: string | null;
+  }[];
+  activity: {
+    type: 'POST_MENTION' | 'COMMENT_MENTION' | 'REPLY_MENTION';
+    sourceUserId: string;
+    sourceId: number;
+    targetId?: number | null;
+    // For 'POST_MENTION', the `targetId` is not needed
+    // For 'COMMENT_MENTION', the `targetId` is the POST `id` of the comment
+    // For 'REPLY_MENTION', the `targetId` is the `parentId` of the reply
+  };
+  isUpdate: boolean;
+}) {
+  // If this is an update operation, delete the previous mention activity logs first
+  if (isUpdate) {
+    await prisma.activity.deleteMany({
+      where: activity,
+    });
+  }
+
+  // Return if there no mentioned users
+  if (!usersMentioned) return;
+
+  // For each mentioned user, create a mention activity
+  for (const { id } of usersMentioned) {
+    await prisma.activity.create({
+      data: { ...activity, targetUserId: id },
+    });
+  }
+}
