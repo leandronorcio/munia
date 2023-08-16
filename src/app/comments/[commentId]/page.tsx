d4aff742fd106ab1fc4fa402bd216386b5891d49
Leandro Norcio <onsongnorcio@gmail.com>
2023-08-16 22:42:00 +0800

    Add a new route, `/comments/:commentId`, which serves the purpose of verifying the existence of a comment. If the comment is found to exist, it will seamlessly redirect the user to the corresponding `/posts/:postId` route.

---
+++ src/app/comments/[commentId]/page.tsx
@@ -0,0 +1,31 @@
+import prisma from '@/lib/prisma/prisma';
+import { redirect } from 'next/navigation';
+
+/**
+ * Use this page to redirect the user to the respective /posts/:postId
+ * route of the comment from the given `commentId`.
+ */
+export default async function Page({
+  params,
+}: {
+  params: { commentId: string };
+}) {
+  const comment = await prisma.comment.findUnique({
+    where: {
+      id: parseInt(params.commentId),
+    },
+    select: {
+      id: true,
+      postId: true,
+      parentId: true,
+    },
+  });
+  if (!comment) return <p>This comment or reply no longer exists.</p>;
+  const { id: commentId, parentId, postId } = comment;
+
+  const searchParams = new URLSearchParams('');
+  searchParams.set('comment-id', commentId.toString());
+  if (parentId) searchParams.set('parent-id', parentId.toString());
+
+  return redirect(`/posts/${postId}?${searchParams.toString()}`);
+}
