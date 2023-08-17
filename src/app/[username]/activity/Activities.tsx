861c78a7f8535f1db03e8f929ff217a59d3896fb
Leandro Norcio <onsongnorcio@gmail.com>
2023-08-17 10:41:48 +0800

    Added front-end for displaying a user's activity logs, these activities, when clicked, should redirect the user to the related profile, post, comment or reply.

---
+++ src/app/[username]/activity/Activities.tsx
@@ -0,0 +1,72 @@
+'use client';
+import { AllCaughtUp } from '@/components/AllCaughtUp';
+import useOnScreen from '@/hooks/useOnScreen';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useEffect, useRef } from 'react';
+import { GetActivity } from 'types';
+import { Activity } from './Activity';
+
+export function Activities({ userId }: { userId: string }) {
+  const bottomElRef = useRef<HTMLDivElement>(null);
+  const isBottomOnScreen = useOnScreen(bottomElRef);
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery<GetActivity[]>({
+    queryKey: ['users', userId, 'activity'],
+    queryFn: async ({ pageParam = 0 }) => {
+      const res = await fetch(
+        `/api/users/${userId}/activity?limit=5&cursor=${pageParam}`,
+      );
+      if (!res.ok) {
+        throw new Error('Failed to load activities.');
+      }
+
+      return (await res.json()) as GetActivity[];
+    },
+    getNextPageParam: (lastPage, pages) => {
+      // If the `pages` `length` is 0, that means there is not a single activity to load
+      if (pages.length === 0) return undefined;
+
+      // If the last page doesn't have activities, that means the end is reached
+      if (lastPage.length === 0) return undefined;
+
+      // Return the id of the last activity, this will serve as the cursor
+      // that will be passed to `queryFn` as the `pageParam` property
+      return lastPage.slice(-1)[0].id;
+    },
+    refetchOnWindowFocus: false,
+  });
+
+  useEffect(() => {
+    if (!isBottomOnScreen) return;
+    if (!data) return;
+    if (!hasNextPage) return;
+
+    fetchNextPage();
+  }, [isBottomOnScreen]);
+
+  return (
+    <div>
+      {data?.pages.flat().map((activity) => {
+        return <Activity key={activity.id} {...activity} />;
+      })}
+
+      <div
+        className="h-6"
+        ref={bottomElRef}
+        /**
+         * The first page will be initially loaded by React Query
+         * so the bottom loader has to be hidden first
+         */
+        style={{ display: data ? 'block' : 'none' }}
+      ></div>
+      {!isFetching && !hasNextPage && <AllCaughtUp />}
+    </div>
+  );
+}
--- src/app/[username]/activity/page.tsx
+++ src/app/[username]/activity/page.tsx
@@ -1,3 +1,17 @@
-export default async function Page() {
-  return <div>This is the activity page.</div>;
+import { getServerUser } from '@/lib/getServerUser';
+import { getProfile } from '../getProfile';
+import { Activities } from './Activities';
+
+export default async function Page({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const [user] = await getServerUser();
+  if (!user) return <p>This is a protected page.</p>;
+  const profile = await getProfile(params.username);
+  const isOwn = user?.id === profile?.id;
+
+  if (!isOwn) return <p>You have no access to this page.</p>;
+  return <Activities userId={user.id} />;
 }
