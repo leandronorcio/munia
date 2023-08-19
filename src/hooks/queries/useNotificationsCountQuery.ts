0eab068c916768b61a3a6af98a17306af693525f
Leandro Norcio <onsongnorcio@gmail.com>
2023-08-19 20:19:28 +0800

    Implement `useNotificationsCountQuery()` hook that refetches the number of unread notifications of the logged in user every 5 seconds.

---
+++ src/hooks/queries/useNotificationsCountQuery.ts
@@ -0,0 +1,21 @@
+import { useQuery } from '@tanstack/react-query';
+import { useSession } from 'next-auth/react';
+
+export function useNotificationsCountQuery() {
+  const { data: session } = useSession();
+  const userId = session?.user.id;
+
+  return useQuery<number>({
+    queryKey: ['users', userId, 'notifications', 'count'],
+    queryFn: async () => {
+      const res = await fetch(`/api/users/${userId}/notifications/count`);
+
+      if (!res.ok) {
+        throw new Error('Error fetching notifications count.');
+      }
+
+      return await res.json();
+    },
+    refetchInterval: 5000,
+  });
+}
