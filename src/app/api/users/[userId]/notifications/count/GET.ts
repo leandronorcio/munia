9c56d0fe894fb047d3c95e33dc92eac0d1af7a3f
Leandro Norcio <onsongnorcio@gmail.com>
2023-08-19 20:13:59 +0800

    Implement GET route handler for getting the number of unread notifications of an authenticated user.

---
+++ src/app/api/users/[userId]/notifications/count/GET.ts
@@ -0,0 +1,26 @@
+/**
+ * GET /api/users/:userId/notifications/count
+ * - Returns the number of the unread notifications of an
+ * authenticated user.
+ */
+import { getServerUser } from '@/lib/getServerUser';
+import prisma from '@/lib/prisma/prisma';
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  const [user] = await getServerUser();
+  if (!user) return NextResponse.json({}, { status: 401 });
+  const userId = user.id;
+
+  const count = await prisma.activity.count({
+    where: {
+      isNotificationRead: false,
+      targetUserId: userId,
+      sourceUserId: {
+        not: userId,
+      },
+    },
+  });
+
+  return NextResponse.json(count);
+}
