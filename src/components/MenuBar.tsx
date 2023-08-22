e30f4b8e1cc2422b0641ac557a603f5ad55f63a5
Leandro Norcio <onsongnorcio@gmail.com>
2023-08-22 11:12:01 +0800

    Fixed <MenuBar> not being overlayed by the dialog overlay on larger screens.

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -19,7 +19,7 @@ export function MenuBar() {
   const { data: notificationCount } = useNotificationsCountQuery();
 
   return (
-    <div className="fixed bottom-0 z-10 flex w-full bg-white/70 shadow-inner backdrop-blur-sm md:sticky md:top-0 md:h-screen md:w-auto md:flex-col md:items-start md:bg-inherit md:p-4 md:shadow-none">
+    <div className="fixed bottom-0 z-[1] flex w-full bg-white/70 shadow-inner backdrop-blur-sm md:sticky md:top-0 md:h-screen md:w-auto md:flex-col md:items-start md:bg-inherit md:p-4 md:shadow-none md:backdrop-blur-none">
       <Link href="/" className="hidden md:block">
         <div className="mb-4 flex items-center gap-2">
           <Logo className="h-12 w-12" />
