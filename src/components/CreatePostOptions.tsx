04ca37eb5cd74edf3ca78c060eafbf17ff6d01bd
Leandro Norcio <onsongnorcio@gmail.com>
2023-08-28 21:21:19 +0800

    Added accessibility to the <CreatePostOptions> component.

--- src/components/CreatePostOptions.tsx
+++ src/components/CreatePostOptions.tsx
@@ -1,37 +1,44 @@
 'use client';
 
-import { EmojiHappySmile, Image } from '@/svg_components';
-import { forwardRef } from 'react';
+import { Image } from '@/svg_components';
+import { forwardRef, useRef } from 'react';
+import { ButtonNaked } from './ui/ButtonNaked';
 
 export const CreatePostOptions = forwardRef<
   HTMLInputElement,
   {
     handleVisualMediaChange: React.ChangeEventHandler<HTMLInputElement>;
   }
->(({ handleVisualMediaChange }, ref) => {
+>(({ handleVisualMediaChange }, forwardedRef) => {
+  const localRef = useRef<HTMLInputElement | null>(null);
+
   return (
     <div className="flex flex-row justify-center gap-6 px-4 pb-5 sm:justify-start">
-      <label className="group flex cursor-pointer flex-row items-center gap-4">
+      <ButtonNaked
+        aria-label="Upload an image or video"
+        className="flex gap-4"
+        onPress={() => localRef.current?.click()}
+      >
         <Image fill="black" width={24} height={24} />
         <p className="text-base font-semibold text-gray-500 group-hover:text-black">
           Image / Video
         </p>
-        <input
-          ref={ref}
-          type="file"
-          className="hidden"
-          name="visualMedia"
-          onChange={handleVisualMediaChange}
-          accept="video/*,.jpg,.jpeg,.png"
-          multiple
-        />
-      </label>
-      <label className="group flex cursor-pointer flex-row items-center gap-4">
-        <EmojiHappySmile stroke="black" width={24} height={24} />
-        <p className="text-base font-semibold text-gray-500 group-hover:text-black">
-          Mood
-        </p>
-      </label>
+      </ButtonNaked>
+      <input
+        ref={(node) => {
+          // https://stackoverflow.com/a/62238917/8434369
+          localRef.current = node;
+          if (typeof forwardedRef === 'function') {
+            forwardedRef(node);
+          } else if (forwardedRef) forwardedRef.current = node;
+        }}
+        type="file"
+        className="hidden"
+        name="visualMedia"
+        onChange={handleVisualMediaChange}
+        accept="video/*,.jpg,.jpeg,.png"
+        multiple
+      />
     </div>
   );
 });
