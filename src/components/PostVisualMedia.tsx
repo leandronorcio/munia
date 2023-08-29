f1f37bd2221b1d03eaa5a96c4259e1a7ea8a461e
Leandro Norcio <onsongnorcio@gmail.com>
2023-08-29 18:50:23 +0800

    The <PostVisualMedia> component is now pressable, focusable and accessible.

--- src/components/PostVisualMedia.tsx
+++ src/components/PostVisualMedia.tsx
@@ -1,6 +1,7 @@
 import { cn } from '@/lib/cn';
 import { Play } from '@/svg_components';
 import { VisualMediaType } from '@prisma/client';
+import { mergeProps, useFocusRing, usePress } from 'react-aria';
 
 export default function PostVisualMedia({
   type,
@@ -11,17 +12,24 @@ export default function PostVisualMedia({
 }: {
   type: VisualMediaType;
   url: string;
-  onClick: React.MouseEventHandler<HTMLDivElement>;
+  onClick: () => void;
   height: string;
   colSpan: number;
 }) {
+  const { pressProps, isPressed } = usePress({
+    onPress: onClick,
+  });
+  const { focusProps, isFocusVisible } = useFocusRing();
   return (
     <div
+      {...mergeProps(pressProps, focusProps)}
+      role="button"
+      tabIndex={0}
       className={cn(
-        'group relative cursor-pointer overflow-hidden',
+        'group relative cursor-pointer focus:outline-none',
         colSpan === 1 ? 'col-span-1' : 'col-span-2',
+        isFocusVisible && 'border-4 border-violet-500',
       )}
-      onClick={onClick}
       style={{
         height,
       }}
@@ -29,7 +37,10 @@ export default function PostVisualMedia({
       {type === 'PHOTO' ? (
         <img
           src={url}
-          className="h-full w-full object-cover transition-transform hover:scale-110"
+          className={cn(
+            'h-full w-full object-cover ',
+            isPressed && 'brightness-75',
+          )}
         />
       ) : (
         <>
