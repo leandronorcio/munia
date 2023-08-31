f3c88a649aa0751d4483742643b8dfd195152a52
Leandro Norcio <onsongnorcio@gmail.com>
2023-09-01 01:36:54 +0800

    Fixed zod `commentWriteSchema` not invalidating inputs that only contain spaces.

--- src/lib/validations/comment.ts
+++ src/lib/validations/comment.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod';
 
 export const commentWriteSchema = z.object({
-  content: z.string().trim(),
+  content: z.string().refine((value) => value.trim().length > 0, {
+    message: 'Content must not be empty or contain only spaces',
+  }),
 });
