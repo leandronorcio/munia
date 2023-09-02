49c8cd5a661f6e5b36ebd34c1cdece64c415a699
Leandro Norcio <onsongnorcio@gmail.com>
2023-09-02 21:52:27 +0800

    Added SVG icons.

---
+++ src/svg_components/BuildingBusinessOffice.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { SVGProps } from "react";
+const SvgBuildingBusinessOffice = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="#1E2028"
+      d="m1 4-.157-.733-.593.127V4H1Zm14 19v.75V23ZM1 23H.25v.75H1V23ZM12.58 1.518l.158.734-.157-.734ZM6 19v-.75h-.75V19H6Zm4 0h.75v-.75H10V19Zm0 4v.75h.75V23H10Zm-4 0h-.75v.75H6V23Zm9-18v-.75a.75.75 0 0 0-.75.75H15Zm8 0h.75a.75.75 0 0 0-.75-.75V5Zm0 18v.75a.75.75 0 0 0 .75-.75H23ZM1.157 4.733l11.581-2.481-.314-1.467L.843 3.267l.314 1.466ZM14.25 3.474V23h1.5V3.474h-1.5ZM15 22.25H1v1.5h14v-1.5ZM1.75 23V4H.25v19h1.5ZM12.738 2.252a1.25 1.25 0 0 1 1.512 1.222h1.5A2.75 2.75 0 0 0 12.424.785l.314 1.467ZM6 19.75h4v-1.5H6v1.5ZM9.25 19v4h1.5v-4h-1.5Zm.75 3.25H6v1.5h4v-1.5ZM6.75 23v-4h-1.5v4h1.5ZM19 14.75h4v-1.5h-4v1.5Zm0-4h4v-1.5h-4v1.5Zm0 8h4v-1.5h-4v1.5Zm-4-13h8v-1.5h-8v1.5ZM22.25 5v18h1.5V5h-1.5ZM23 22.25h-8v1.5h8v-1.5Zm-7.25.75V5h-1.5v18h1.5Zm2.5-23v5h1.5V0h-1.5ZM4 15.75h3v-1.5H4v1.5Zm5 0h3v-1.5H9v1.5Zm0-8h3v-1.5H9v1.5Zm-5 0h3v-1.5H4v1.5Zm0 4h3v-1.5H4v1.5Zm5 0h3v-1.5H9v1.5Z"
+    />
+  </svg>
+);
+export default SvgBuildingBusinessOffice;
