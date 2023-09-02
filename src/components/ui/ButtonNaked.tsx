05428ccda78c8d94a9a3e468803cd6619f7f3d1a
Leandro Norcio <onsongnorcio@gmail.com>
2023-09-02 20:45:55 +0800

    Implement forward ref support to the <ButtonNaked> component.

--- src/components/ui/ButtonNaked.tsx
+++ src/components/ui/ButtonNaked.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { cn } from '@/lib/cn';
-import { useRef } from 'react';
+import { useObjectRef } from '@react-aria/utils';
+import { forwardRef } from 'react';
 import {
   AriaButtonProps,
   mergeProps,
@@ -13,22 +14,25 @@ interface ButtonProps extends AriaButtonProps {
   children: React.ReactNode;
   className?: string;
 }
-export function ButtonNaked({ className, children, ...rest }: ButtonProps) {
-  const ref = useRef(null);
-  const { buttonProps } = useButton({ ...rest }, ref);
-  const { isFocusVisible, focusProps } = useFocusRing();
 
-  return (
-    <button
-      {...mergeProps(buttonProps, focusProps)}
-      ref={ref}
-      className={cn(
-        'rounded-sm focus:outline-none',
-        className,
-        isFocusVisible && 'ring ring-violet-500 ring-offset-2',
-      )}
-    >
-      {children}
-    </button>
-  );
-}
+export const ButtonNaked = forwardRef<HTMLButtonElement, ButtonProps>(
+  function ButtonNaked({ className, children, ...rest }, forwardedRef) {
+    const ref = useObjectRef(forwardedRef);
+    const { buttonProps } = useButton({ ...rest }, ref);
+    const { isFocusVisible, focusProps } = useFocusRing();
+
+    return (
+      <button
+        {...mergeProps(buttonProps, focusProps)}
+        ref={ref}
+        className={cn(
+          'rounded-sm focus:outline-none',
+          className,
+          isFocusVisible && 'ring ring-violet-500 ring-offset-2',
+        )}
+      >
+        {children}
+      </button>
+    );
+  },
+);
