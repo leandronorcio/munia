2744985734bef65b21a9256359e7334c11d9e5c0
Leandro Norcio <onsongnorcio@gmail.com>
2023-09-08 20:45:46 +0800

    * Adjusted the CSS variables. * Added more CSS variables.

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
         'popover-foreground': 'rgba(var(--popover-foreground) / <alpha-value>)',
         primary: 'rgba(var(--primary) / <alpha-value>)',
         'primary-foreground': 'rgba(var(--primary-foreground) / <alpha-value>)',
+        'primary-accent': 'rgba(var(--primary-accent) / <alpha-value>)',
         secondary: 'rgba(var(--secondary) / <alpha-value>)',
         'secondary-foreground':
           'rgba(var(--secondary-foreground) / <alpha-value>)',
@@ -24,6 +25,10 @@ module.exports = {
         'muted-foreground': 'rgba(var(--muted-foreground) / <alpha-value>)',
         accent: 'rgba(var(--accent) / <alpha-value>)',
         'accent-foreground': 'rgba(var(--accent-foreground) / <alpha-value>)',
+        success: 'rgba(var(--success) / <alpha-value>)',
+        'success-foreground': 'rgba(var(--success-foreground) / <alpha-value>)',
+        warning: 'rgba(var(--warning) / <alpha-value>)',
+        'warning-foreground': 'rgba(var(--warning-foreground) / <alpha-value>)',
         destructive: 'rgba(var(--destructive) / <alpha-value>)',
         'destructive-foreground':
           'rgba(var(--destructive-foreground) / <alpha-value>)',
