5cca1ade0aa46c812ace744b6b535dac127e4533
Leandro Norcio <onsongnorcio@gmail.com>
2023-09-09 01:28:37 +0800

    Added <SomethingWentWrong> component.

---
+++ src/components/SometingWentWrong.tsx
@@ -0,0 +1,20 @@
+import { CircleActionsAlertInfo } from '@/svg_components';
+
+export function SomethingWentWrong() {
+  return (
+    <div className="mt-6 grid place-items-center">
+      <div className="inline-block rounded-xl bg-destructive px-8 py-6">
+        <div className="flex items-center gap-4">
+          <CircleActionsAlertInfo
+            className="stroke-destructive-foreground"
+            width={24}
+            height={24}
+          />
+          <p className="text-lg font-semibold text-destructive-foreground">
+            Something went wrong.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
