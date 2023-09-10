2a7399228396addf0c74f3b55ec4bbcf233e7171
Leandro Norcio <onsongnorcio@gmail.com>
2023-09-11 00:06:13 +0800

    Minor cleanup to `/posts/`.

--- src/app/(main)/posts/[postId]/page.tsx
+++ src/app/(main)/posts/[postId]/page.tsx
@@ -1,31 +1,12 @@
 'use client';
 
 import { Post } from '@/components/Post';
-import { useDeletePostMutation } from '@/hooks/mutations/useDeletePostMutation';
-import { useToast } from '@/hooks/useToast';
-import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 export default function Page({ params }: { params: { postId: string } }) {
   const postId = parseInt(params.postId);
   const [commentsShown, setCommentsShown] = useState(true);
-  const { deleteMutation } = useDeletePostMutation();
-  const { showToast } = useToast();
-  const router = useRouter();
 
-  const deletePost = () => {
-    deleteMutation.mutate(
-      { postId },
-      {
-        onSuccess: () => {
-          router.back();
-          showToast({
-            title: 'Successfully Deleted',
-          });
-        },
-      },
-    );
-  };
   const toggleComments = () => setCommentsShown((prev) => !prev);
 
   return (
