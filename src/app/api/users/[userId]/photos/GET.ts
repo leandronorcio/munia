6a31fe7e555cbce310b8808734cb769d72267fbd
Leandro Norcio <onsongnorcio@gmail.com>
2023-10-24 00:51:17 +0800

    Added route handler for getting a user's visual media URLs.

---
+++ src/app/api/users/[userId]/photos/GET.ts
@@ -0,0 +1,30 @@
+/**
+ * GET /api/users/:userId/photos
+ * - Returns the visual media URLs of the specified user.
+ */
+
+import prisma from '@/lib/prisma/prisma';
+import { fileNameToUrl } from '@/lib/s3/fileNameToUrl';
+import { NextResponse } from 'next/server';
+import { GetVisualMedia } from 'types';
+
+export async function GET(
+  request: Request,
+  { params }: { params: { userId: string } },
+) {
+  const res = await prisma.visualMedia.findMany({
+    where: {
+      userId: params.userId,
+    },
+    orderBy: {
+      id: 'desc',
+    },
+  });
+
+  const visualMedia: GetVisualMedia[] | null = res.map((item) => ({
+    type: item.type,
+    url: fileNameToUrl(item.fileName)!,
+  }));
+
+  return NextResponse.json(visualMedia);
+}
