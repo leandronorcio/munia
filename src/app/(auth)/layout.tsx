09cc3f61cc0cadf9766cb7a8b705e4cbe7083562
Leandro Norcio <onsongnorcio@gmail.com>
2023-11-09 22:55:21 +0800

    Redesigned the home page.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,18 +1,6 @@
-import { LogoText } from '@/components/LogoText';
-import { ButtonLink } from '@/components/ui/ButtonLink';
-import { ArrowChevronBack, BackArrow } from '@/svg_components';
-import SvgLogo from '@/svg_components/Logo';
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div className="fixed top-4 flex w-full justify-between pr-6 sm:top-8 sm:px-10">
-        <div>
-          <ButtonLink href="/" mode="ghost" Icon={ArrowChevronBack}>
-            Home
-          </ButtonLink>
-        </div>
-      </div>
       <div className="grid h-screen place-items-center">
         <div className="w-[320px] md:w-[428px]">{children}</div>
       </div>
--- src/app/(unprotected)/page.tsx
+++ src/app/(unprotected)/page.tsx
@@ -1,26 +1,35 @@
-import { Feather, LogInSquare, TwoPeople } from '@/svg_components';
+'use client';
+import { Feather, Github, LogInSquare } from '@/svg_components';
 import { ButtonLink } from '@/components/ui/ButtonLink';
 import { LogoText } from '@/components/LogoText';
 
 export default function Page() {
   return (
     <main className="relative flex min-h-screen w-full flex-col items-center justify-center">
-      <div className="absolute right-10 top-10 flex flex-row gap-4">
-        <ButtonLink href="/login" size="medium" shape="pill" Icon={LogInSquare}>
-          Login
-        </ButtonLink>
-        <ButtonLink
-          href="/register"
-          size="medium"
-          shape="pill"
-          Icon={TwoPeople}
-        >
-          Sign Up
-        </ButtonLink>
-      </div>
-      <div className="flex h-56 w-full flex-row items-center justify-center gap-4 rounded-none bg-card md:w-[550px] md:rounded-3xl">
-        <Feather className="stroke-primary" width={88} height={88} />
-        <LogoText className="text-6xl" />
+      <div className="max-w-3xl">
+        <div className="rounded-none py-10 md:rounded-3xl">
+          <div className="flex w-full flex-row items-center justify-center gap-4">
+            <Feather className="stroke-primary" width={80} height={80} />
+            <LogoText className="text-6xl" />
+          </div>
+          <p className="mt-4 px-5 text-center text-2xl text-muted-foreground sm:mt-6 sm:text-4xl">
+            An open source, responsive and accessible full stack social media
+            web app built with Next.js.
+          </p>
+          <div className="mt-6 flex justify-center gap-3">
+            <ButtonLink href="/login" size="medium" Icon={LogInSquare}>
+              Get Started
+            </ButtonLink>
+            <ButtonLink
+              href="https://github.com/leandronorcio/munia"
+              size="medium"
+              mode="subtle"
+              Icon={Github}
+            >
+              Github
+            </ButtonLink>
+          </div>
+        </div>
       </div>
     </main>
   );
