5738b1a7d2ba0a29665ce4cfbd65b40427da039e
Leandro Norcio <onsongnorcio@gmail.com>
2023-11-10 00:39:05 +0800

    Ignore ts checks.

--- src/lib/prisma/prisma.ts
+++ src/lib/prisma/prisma.ts
@@ -15,12 +15,14 @@ if (typeof window === 'undefined') {
   if (process.env.NODE_ENV === 'production') {
     prisma = new PrismaClient();
   } else {
+    // @ts-ignore
     if (!global.prisma) {
+      // @ts-ignore
       global.prisma = new PrismaClient();
     }
-
+    // @ts-ignore
     prisma = global.prisma;
   }
 }
-
+// @ts-ignore
 export default prisma;
