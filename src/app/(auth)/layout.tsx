d10ca3c3f8e111379b4efa8316290e1b899417c1
Leandro Norcio <onsongnorcio@gmail.com>
2023-11-11 14:01:14 +0800

    Remove unnecessary fragment.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <div className="grid h-screen place-items-center">
-        <div className="w-[320px] md:w-[428px]">{children}</div>
-      </div>
-    </>
+    <div className="grid h-screen place-items-center">
+      <div className="w-[320px] md:w-[428px]">{children}</div>
+    </div>
   );
 }
