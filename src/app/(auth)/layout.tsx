5688e8091442772bfaccfe47b97b6c494d13903a
Leandro Norcio <onsongnorcio@gmail.com>
2023-11-17 21:01:38 +0800

    Added 'Back to Home' button to auth pages.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,17 @@
+import { ArrowChevronBack } from '@/svg_components';
+import Link from 'next/link';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid h-screen place-items-center">
+    <div className="relative grid h-screen place-items-center">
+      <div className="absolute left-3 top-4 sm:left-6 sm:top-6">
+        <Link href="/" className="group flex items-center gap-2 sm:gap-4">
+          <ArrowChevronBack className="stroke-foreground group-hover:stroke-muted-foreground" />
+          <span className="group-hover:text-muted-foreground">
+            Back to Home
+          </span>
+        </Link>
+      </div>
       <div className="w-[320px] md:w-[428px]">{children}</div>
     </div>
   );
