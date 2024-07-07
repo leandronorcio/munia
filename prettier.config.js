11db4f4a75ac05b4c7844a2070c200e6751f37b8
Leandro Norcio <onsongnorcio@gmail.com>
2024-07-07 16:08:54 +0800

    chore(prettier-tailwind): 🎨 Add custom tailwind class names, so that these class names receive tailwind intellisense and are auto formatted.

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: ['prettier-plugin-tailwindcss'],
   tailwindFunctions: ['clsx', 'cva'],
+  tailwindAttributes: ['class', 'className', 'iconClassName'],
   singleQuote: true,
   trailingComma: 'all',
 };
