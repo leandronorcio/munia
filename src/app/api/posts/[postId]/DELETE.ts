67f9cbda49325ebccd919271f36f855fae069fce
saponalisaju <saponalibpl12486@gmail.com>
2025-12-09 15:22:03 +0600

    ;;

--- src/app/api/posts/[postId]/DELETE.ts
+++ src/app/api/posts/[postId]/DELETE.ts
@@ -7,26 +7,26 @@ import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma/prisma';
 import { deleteObject } from '@/lib/s3/deleteObject';
 import { verifyAccessToPost } from './verifyAccessToPost';
+import { VisualMedia } from '@prisma/client'; // import the type from Prisma
 
 export async function DELETE(request: Request, { params }: { params: { postId: string } }) {
   const postId = parseInt(params.postId, 10);
+
   if (!verifyAccessToPost(postId)) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 403 });
   }
 
-  // Delete the `post` and the associated `visualMedia` from the database
+  // Delete the post and retrieve the associated visualMedia
   const res = await prisma.post.delete({
+    where: { id: postId },
     select: {
       id: true,
       visualMedia: true,
     },
-    where: {
-      id: postId,
-    },
   });
 
-  // Delete the associated `visualMedia` files from the S3 bucket
-  const filenames = res.visualMedia.map((m) => m.fileName);
+  // Delete the associated visualMedia files from S3
+  const filenames = res.visualMedia.map((m: VisualMedia) => m.fileName);
   await Promise.all(filenames.map(deleteObject));
 
   return NextResponse.json({ id: res.id });
